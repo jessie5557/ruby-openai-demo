50a46c460cba07576241f59782fe4287f4e7f4ef
jessie5557 <ychen38@chicagobooth.edu>
2025-01-29 04:27:57 +0000

    raw reponse done

--- challenge.rb
+++ challenge.rb
@@ -6,3 +6,20 @@ client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_KEY"))
 pp "Hello! How can I help you today?"
 pp "-" * 50
 user_request = gets
+
+# Prepare an Array of previous messages
+message_list = [
+  {:role => "system",:content => "You are a helpful assistant."},
+  {:role => "user",:content => user_request}
+  ]  
+
+raw_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+
+pp raw_response
+
+next_respons = p
